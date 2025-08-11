7fde79c49ab929078eda9d0a6e4ce1001dca0224
deey001 <dvillazon@gmail.com>
2025-08-10 21:02:58 -0400

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# uninstall.sh - Remove dotfiles symlinks and clean up
-
-# Remove symlinks
 echo "Removing symlinks..."
 rm -f "$HOME/.bash_aliases"
 rm -f "$HOME/.bash_exports"
@@ -12,15 +9,4 @@ rm -f "$HOME/.bashrc"
 rm -f "$HOME/.tmux.conf"
 rm -f "$HOME/.config/starship.toml"
 
-# Clean up tmux plugins (optional: uncomment if desired)
-# rm -rf "$HOME/.tmux/plugins"
-
-# Optionally remove dependencies (uncomment with caution)
-# if [ "$(uname)" = "Darwin" ]; then
-#   brew uninstall tmux git fzf starship
-# elif [ "$(uname)" = "Linux" ]; then
-#   sudo apt remove -y tmux git fzf xclip wl-clipboard
-#   # Starship uninstall: rm ~/.cargo/bin/starship if installed via curl
-# fi
-
-echo "Uninstall complete! You may need to restart your shell."
+echo "Uninstall complete! Restart shell."
